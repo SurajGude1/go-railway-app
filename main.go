83da040a8f70889b7ad9cf30f4327985ba78599a
SurajGude1 <surajgude2001@gmail.com>
2025-07-06 11:43:33 +0530

    changes related to production

--- main.go
+++ main.go
@@ -18,9 +18,9 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://next-vercel-app-ten.vercel.app/"},
-		AllowMethods:     []string{"GET"},
-		AllowHeaders:     []string{"Origin", "Content-Type"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://next-vercel-app-ten.vercel.app"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
 		AllowCredentials: true,
 	}))
 
